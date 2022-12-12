889c3d467942d7c646ca647e62fa6f270fa42e88
Umarbek Rakhmatov <umarbek1306@gmail.com>
2022-12-12 16:14:20 -0500

    commit tree 5d2e37404123d168b8d8269b5711b1fb4bafd587

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop("Ralph");
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift("Bob");
+}
+
+function appendCat() {
+    const copyOfCats = [...cats, "Broom"]; return copyOfCats
+}
+
+function prependCat() {
+    const copyOfCats = ["Arnold", ...cats]; return copyOfCats
+}
+
+function removeLastCat() {
+    const copyOfPrimeCats = cats.slice(0, cats.length -1); return copyOfPrimeCats
+}
+function removeFirstCat () {
+    const copyOfPrimeCats = cats.slice (1); return copyOfPrimeCats
+}
